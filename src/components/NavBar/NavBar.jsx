c33a5ef897eee38de0be841417295c9d4c9ec60f
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-18 02:49:38 +0600

    created Nav toggle

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+// import React from 'react';
+import { useState } from 'react';
 import Link from '../Link/Link';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { ImCross } from 'react-icons/im';
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: 'Home', path: '/' },
     { id: 2, name: 'About', path: '/about' },
@@ -11,6 +15,12 @@ const NavBar = () => {
   ];
   return (
     <nav>
+      <div
+        className="md:hidden cursor-pointer text-2xl"
+        onClick={() => setOpen(!open)}
+      >
+        {open === true ? <ImCross></ImCross> : <AiOutlineMenu></AiOutlineMenu>}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
